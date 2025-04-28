4f98f35a95fae643a159eaf6e2e2d01d2cd4a51b
정영하 <pd.hyazzang@gmail.com>
2025-04-28 18:34:54 +0900

    ✨ Post api
    post api 기능

--- apps/post/serializers.py
+++ apps/post/serializers.py
@@ -3,7 +3,14 @@ from rest_framework import serializers
 from apps.user.serializers import UserSerializer
 
 from .models import Post
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
 
 class PostSerializer(serializers.ModelSerializer):
     """게시물 시리얼라이저"""
@@ -41,3 +48,9 @@ class PostUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ["title", "content", "image"]
+
+    def validate_image_url(self, value):
+        """이미지 URL 유효성 검사"""
+        if value and not value.startswith(('http://', 'https://')):
+            raise serializers.ValidationError("올바른 URL 형식이어야 합니다.")
+        return value
--- apps/post/urls.py
+++ apps/post/urls.py
@@ -14,4 +14,15 @@ router.register(r"posts", PostViewSet, basename="post")
 urlpatterns = [
     # 게시물 관련 엔드포인트
     path("", include(router.urls)),
+    # 게시물 목록 조회 및 생성
+    path('', PostListCreateView.as_view(), name='list-create'),
+
+    # 특정 게시물 조회, 수정, 삭제
+    path('<int:pk>/', PostRetrieveUpdateDestroyView.as_view(), name='detail'),
+
+    # 게시물 조회수 증가
+    path('<int:pk>/increase-views/', PostIncreaseViewsView.as_view(), name='increase-views'),
+
+    # 작성자별 게시물 목록 조회
+    path('author/<int:author_id>/', PostListByAuthorView.as_view(), name='list-by-author'),
 ]
