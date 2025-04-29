f58c921b78730cc1f18e115f7aebe0e1bed163ac
LucasKim4321 <xowls0131@naver.com>
2025-04-29 21:06:11 +0900

    🚑hotfix : 배포 설정 수정

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -4,15 +4,15 @@ from dotenv import dotenv_values
 
 from .base import *  # noqa: F403
 
-# DEBUG = True
-# ALLOWED_HOSTS: list[str] = []
+DEBUG = True
+ALLOWED_HOSTS: list[str] = []
 
-DEBUG = False
-ALLOWED_HOSTS: list[str] = [
-    "127.0.0.1",
-    "localhost",
-    "3.39.10.22",  # EC2 퍼블릭 IP
-]
+# DEBUG = False
+# ALLOWED_HOSTS: list[str] = [
+#     "127.0.0.1",
+#     "localhost",
+#     "3.39.10.22",  # EC2 퍼블릭 IP
+# ]
 
 # 시크릿 키를 ENV 변수에 저장된 딕셔너리에서 가져옵니다. 만약 파일에서 읽어온 시크릿 키가 존재하지 않는다면 50자리의 무작위 문자열을 반환합니다.
 SECRET_KEY: str = os.getenv(
