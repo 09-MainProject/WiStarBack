124a306c1c3df2a76997360bbea736978d54baff
정영하 <pd.hyazzang@gmail.com>
2025-04-29 20:12:56 +0900

    ⚡️ Feat: comment api
    comment api 작성

---
+++ apps/comment/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from .models import Comment
+from apps.user.serializers import UserSerializer
+
+class CommentSerializer(serializers.ModelSerializer):
+    """댓글 시리얼라이저"""
+    author = UserSerializer(read_only=True)
+    
+    class Meta:
+        model = Comment
+        fields = ['id', 'post', 'author', 'content', 'created_at', 'updated_at', 'is_deleted']
+        read_only_fields = ['author', 'created_at', 'updated_at', 'is_deleted']
+
+class CommentCreateSerializer(serializers.ModelSerializer):
+    """댓글 생성 시리얼라이저"""
+    class Meta:
+        model = Comment
+        fields = ['post', 'content']
+
+class CommentUpdateSerializer(serializers.ModelSerializer):
+    """댓글 수정 시리얼라이저"""
+    class Meta:
+        model = Comment
+        fields = ['content'] 
---
+++ apps/comment/views.py
@@ -0,0 +1,78 @@
+from rest_framework import viewsets, status, filters
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
+from django_filters import rest_framework as django_filters
+from .models import Comment
+from .serializers import CommentSerializer, CommentCreateSerializer, CommentUpdateSerializer
+from apps.post.models import Post
+
+class CommentFilter(django_filters.FilterSet):
+    """댓글 필터"""
+    content = django_filters.CharFilter(lookup_expr='icontains')
+    created_at = django_filters.DateTimeFilter(lookup_expr='gte')
+    created_at_end = django_filters.DateTimeFilter(field_name='created_at', lookup_expr='lte')
+
+    class Meta:
+        model = Comment
+        fields = ['content', 'created_at', 'created_at_end']
+
+class CommentViewSet(viewsets.ModelViewSet):
+    """댓글 뷰셋"""
+    permission_classes = [IsAuthenticated]
+    filter_backends = [django_filters.DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_class = CommentFilter
+    search_fields = ['content']
+    ordering_fields = ['created_at']
+    ordering = ['-created_at']
+    
+    def get_queryset(self):
+        """댓글 목록을 반환합니다."""
+        post_id = self.kwargs.get('post_pk')
+        return Comment.objects.filter(post_id=post_id, is_deleted=False)
+    
+    def get_serializer_class(self):
+        """액션에 따라 적절한 시리얼라이저를 반환합니다."""
+        if self.action == 'create':
+            return CommentCreateSerializer
+        elif self.action in ['update', 'partial_update']:
+            return CommentUpdateSerializer
+        return CommentSerializer
+    
+    def perform_create(self, serializer):
+        """댓글을 생성합니다."""
+        post_id = self.kwargs.get('post_pk')
+        post = get_object_or_404(Post, id=post_id)
+        serializer.save(author=self.request.user, post=post)
+    
+    def perform_update(self, serializer):
+        """댓글을 수정합니다."""
+        comment = self.get_object()
+        if comment.author != self.request.user:
+            return Response(
+                {"detail": "댓글을 수정할 권한이 없습니다."},
+                status=status.HTTP_403_FORBIDDEN
+            )
+        serializer.save()
+    
+    def perform_destroy(self, instance):
+        """댓글을 소프트 삭제합니다."""
+        if instance.author != self.request.user:
+            return Response(
+                {"detail": "댓글을 삭제할 권한이 없습니다."},
+                status=status.HTTP_403_FORBIDDEN
+            )
+        instance.soft_delete(self.request.user)
+    
+    @action(detail=True, methods=['post'])
+    def restore(self, request, pk=None, post_pk=None):
+        """삭제된 댓글을 복구합니다."""
+        comment = self.get_object()
+        if comment.author != request.user:
+            return Response(
+                {"detail": "댓글을 복구할 권한이 없습니다."},
+                status=status.HTTP_403_FORBIDDEN
+            )
+        comment.restore()
+        return Response(CommentSerializer(comment).data) 
