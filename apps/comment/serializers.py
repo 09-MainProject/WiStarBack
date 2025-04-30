a4c2a10f09590704d9b9acc6f906fb828a154ed9
정영하 <pd.hyazzang@gmail.com>
2025-04-30 11:23:47 +0900

    🐛 Fix: comment api
    ci

--- apps/comment/serializers.py
+++ apps/comment/serializers.py
@@ -2,26 +2,40 @@ from rest_framework import serializers
 from .models import Comment
 from apps.user.serializers import UserSerializer
 
+
 class CommentSerializer(serializers.ModelSerializer):
     """댓글 시리얼라이저"""
+
     author = UserSerializer(read_only=True)
-    
+
     class Meta:
         model = Comment
-        fields = ['id', 'post', 'author', 'content', 'created_at', 'updated_at', 'is_deleted']
-        read_only_fields = ['author', 'created_at', 'updated_at', 'is_deleted']
+        fields = [
+            "id",
+            "post",
+            "author",
+            "content",
+            "created_at",
+            "updated_at",
+            "is_deleted",
+        ]
+        read_only_fields = ["author", "created_at", "updated_at", "is_deleted"]
+
 
 class CommentCreateSerializer(serializers.ModelSerializer):
     """댓글 생성 시리얼라이저"""
+
     author = UserSerializer(read_only=True)
-    
+
     class Meta:
         model = Comment
-        fields = ['content', 'author']
-        read_only_fields = ['author']
+        fields = ["content", "author"]
+        read_only_fields = ["author"]
+
 
 class CommentUpdateSerializer(serializers.ModelSerializer):
     """댓글 수정 시리얼라이저"""
+
     class Meta:
         model = Comment
-        fields = ['content'] 
+        fields = ["content"]
