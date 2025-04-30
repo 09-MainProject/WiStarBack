ac7479a164590ed9b59fe30f57170329de6551f8
정영하 <pd.hyazzang@gmail.com>
2025-04-30 12:10:52 +0900

    🐛 Fix: formatting
    ci

--- apps/comment/views.py
+++ apps/comment/views.py
@@ -6,13 +6,14 @@ from rest_framework.exceptions import PermissionDenied
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 
+from apps.post.models import Post
+
 from .models import Comment
 from .serializers import (
     CommentCreateSerializer,
     CommentSerializer,
     CommentUpdateSerializer,
 )
-from apps.post.models import Post
 
 
 class CommentFilter(django_filters.FilterSet):
