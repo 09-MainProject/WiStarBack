c7a142394d1191092c771dafb6a032e3b58184fc
정영하 <pd.hyazzang@gmail.com>
2025-04-30 11:30:51 +0900

    🐛 Fix: apply black formatting
    formatting

---
+++ apps/like/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path
+from .views import LikeViewSet
+
+like_list = LikeViewSet.as_view(
+    {
+        "get": "list",
+        "post": "create",
+    }
+)
+like_detail = LikeViewSet.as_view(
+    {
+        "delete": "destroy",
+    }
+)
+like_status = LikeViewSet.as_view(
+    {
+        "get": "like_status",
+    }
+)
+
+urlpatterns = [
+    path("<str:type>/<int:id>/like-status", like_status, name="like-status"),
+    path("<str:type>/<int:id>/likes", like_list, name="like-list"),
+    path("<str:type>/<int:id>/likes/<int:pk>", like_detail, name="like-detail"),
+]
