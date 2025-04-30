9087688b7be3051fce6379b5ad62d44e1fd7e6b1
정영하 <pd.hyazzang@gmail.com>
2025-04-30 11:33:50 +0900

    🐛 Fix: apply isort formatting
    isort formatting

--- apps/comment/tests.py
+++ apps/comment/tests.py
@@ -1,9 +1,11 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
-from django.contrib.auth import get_user_model
+
 from apps.post.models import Post
+
 from .models import Comment
 
 User = get_user_model()
