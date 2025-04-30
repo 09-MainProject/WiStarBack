c0ea39dccb1a7974c28762cd65f5bc0da142a15b
정영하 <pd.hyazzang@gmail.com>
2025-04-30 11:55:43 +0900

    🐛 Fix: ci
    ci ci ci ci

--- apps/post/models.py
+++ apps/post/models.py
@@ -1,7 +1,6 @@
 from io import BytesIO
 
 import requests
-from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils import timezone
