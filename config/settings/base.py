aa3f84b9e6a1f8d404ec2b8e19e6831c94a0e8a5
LucasKim4321 <xowls0131@naver.com>
2025-05-08 03:47:49 +0900

    🐛fix : memo

--- config/settings/base.py
+++ config/settings/base.py
@@ -162,8 +162,16 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATIC_DIR = BASE_DIR / "static"
 
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
+
 # 배포할 때 사용하는 경로
 STATIC_ROOT = BASE_DIR / ".static_root"
+
+# Media
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
