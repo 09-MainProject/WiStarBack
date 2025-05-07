e7e9149dafc03c01f0f0bc53947c65e99896b8fb
gkw314 <go980314@gmail.com>
2025-05-08 04:56:34 +0900

    🐛 fix : 병합 출동 해결

--- apps/idol_schedule/serializers.py
+++ apps/idol_schedule/serializers.py
@@ -2,8 +2,6 @@ from rest_framework import serializers
 
 from .models import Idol, Schedule
 
-
-# 실제사용
 class ScheduleSerializer(serializers.ModelSerializer):
     idol_name = serializers.CharField(source="idol.name", read_only=True)
 
@@ -35,12 +33,3 @@ class ScheduleSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError("시작일은 종료일보다 빨라야 합니다.")
 
         return data
-
-
-# 테스트용
-# class ScheduleSerializer(serializers.ModelSerializer):
-#     idol = serializers.PrimaryKeyRelatedField(queryset=Idol.objects.all(), read_only=True)  # read_only로 설정
-#
-#     class Meta:
-#         model = Schedule
-#         fields = '__all__'
