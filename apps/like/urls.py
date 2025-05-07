c80bbfb71a564818c6dc3056ea9bd088109b2bd5
정영하 <pd.hyazzang@gmail.com>
2025-05-07 15:02:05 +0900

    🐛 Fix: comment, like modify the URL path
    modify the URL path

--- apps/like/urls.py
+++ apps/like/urls.py
@@ -21,15 +21,17 @@ like_status = LikeViewSet.as_view(
 
 urlpatterns = [
     path(
-        "<str:app_label>/<str:model>/<int:object_id>/like-status",
+        "<str:app_label>/<int:object_id>/like-status",
         like_status,
         name="like-status",
     ),
     path(
-        "<str:app_label>/<str:model>/<int:object_id>/likes", like_list, name="like-list"
+        "<str:app_label>/<int:object_id>/likes",
+        like_list,
+        name="like-list"
     ),
     path(
-        "<str:app_label>/<str:model>/<int:object_id>/likes/<int:pk>",
+        "<str:app_label>/<int:object_id>/likes/<int:pk>",
         like_detail,
         name="like-detail",
     ),
