ee4a52bcd2f377c1d08e46a8a98f9ba4e3391efa
gkw314 <go980314@gmail.com>
2025-05-07 17:09:08 +0900

    🐛 fix: 아이돌 스케줄 기능 수정 및 테스트

--- apps/idol_schedule/urls.py
+++ apps/idol_schedule/urls.py
@@ -5,13 +5,13 @@ from .views import ScheduleListCreateView, ScheduleRetrieveUpdateDeleteView
 urlpatterns = [
     # 아이돌 ID를 URL에 포함시켜서 일정 목록을 조회하거나 생성
     path(
-        "idols/<int:idol_id>/schedules/",
+        "<int:idol_id>/schedules/",
         ScheduleListCreateView.as_view(),
         name="schedule-list-create",
     ),
     # 특정 일정에 대해 조회, 수정, 삭제
     path(
-        "idols/<int:idol_id>/schedules/<int:pk>/",
+        "<int:idol_id>/schedules/<int:pk>/",
         ScheduleRetrieveUpdateDeleteView.as_view(),
         name="schedule-retrieve-update-delete",
     ),
