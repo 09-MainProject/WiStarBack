961b46a590aa516616b25c310d6285343a158026
LucasKim4321 <xowls0131@naver.com>
2025-05-08 01:01:08 +0900

    🐛fix : 배포용 설정

--- config/settings/__init__.py
+++ config/settings/__init__.py
@@ -1,6 +1,8 @@
-from .local import *  # noqa: F403
+# from .local import *  # noqa: F403
 
-# from .prod import *  # noqa: F403
+from .prod import *  # noqa: F403
+
+# from .prod_for_docker import *  # noqa: F403
 
 # print(f"DEBUG = {DEBUG}")
 # print(ENV.get("DJANGO_SECRET_KEY"))
--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,6 @@
-# ruff: noqa: F405
-
 import random
+
+from dotenv import dotenv_values
 
 from .base import *  # noqa: F403
 
@@ -11,11 +11,16 @@ DEBUG = False
 ALLOWED_HOSTS: list[str] = [
     "127.0.0.1",
     "localhost",
-    "3.39.10.22",  # EC2 퍼블릭 IP
+    "43.203.181.6",
 ]
 
+
+# dotenv_values 메서드는 env 파일의 경로를 파라미터로 전달 받아 해당 파일을 읽어온 후 Key, Value 형태로 매핑하여 dict로 반환합니다.
+# ENV = dotenv_values(BASE_DIR / "envs/local.env")  # noqa: F405
+ENV = dotenv_values(BASE_DIR / "envs/.env.prod")  # noqa: F405
+
 # 시크릿 키를 ENV 변수에 저장된 딕셔너리에서 가져옵니다. 만약 파일에서 읽어온 시크릿 키가 존재하지 않는다면 50자리의 무작위 문자열을 반환합니다.
-SECRET_KEY: str = os.getenv(
+SECRET_KEY: str = ENV.get(
     "DJANGO_SECRET_KEY",
     "".join(random.choices("abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()?", k=50)),
 )
@@ -23,11 +28,11 @@ SECRET_KEY: str = os.getenv(
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "HOST": os.getenv("POSTGRES_HOST", "db"),
-        "USER": os.getenv("POSTGRES_USER", "postgres"),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "postgres"),
-        "NAME": os.getenv("POSTGRES_DB", "oz_collabo"),
-        "PORT": os.getenv("POSTGRES_PORT", 5432),
+        "HOST": ENV.get("POSTGRES_HOST", "db"),
+        "USER": ENV.get("POSTGRES_USER", "postgres"),
+        "PASSWORD": ENV.get("POSTGRES_PASSWORD", "postgres"),
+        "NAME": ENV.get("POSTGRES_DB", "oz_collabo"),
+        "PORT": ENV.get("POSTGRES_PORT", 5432),
     }
 }
 
@@ -38,10 +43,16 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.naver.com"  # 네이버 환결설정에서 볼 수 있음.
 EMAIL_USE_TLS = True  # 보안연결
 EMAIL_PORT = 587  # 네이버 메일 환경설정에서 확인 가능
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
+EMAIL_HOST_USER = ENV.get("EMAIL_HOST_USER", "")
+EMAIL_HOST_PASSWORD = ENV.get("EMAIL_HOST_PASSWORD", "")
 
 
 # OAuth
-NAVER_CLIENT_ID = os.getenv("NAVER_CLIENT_ID", "")
-NAVER_CLIENT_SECRET = os.getenv("NAVER_CLIENT_SECRET", "")
+NAVER_CLIENT_ID = ENV.get("NAVER_CLIENT_ID", "")
+NAVER_CLIENT_SECRET = ENV.get("NAVER_CLIENT_SECRET", "")
+
+KAKAO_REST_API_KEY = ENV.get("KAKAO_REST_API_KEY", "")
+KAKAO_CLIENT_SECRET = ENV.get("KAKAO_CLIENT_SECRET", "")
+
+GOOGLE_CLIENT_ID = ENV.get("GOOGLE_CLIENT_ID", "")
+GOOGLE_CLIENT_SECRET = ENV.get("GOOGLE_CLIENT_SECRET", "")
