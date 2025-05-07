54c1a5faf8f64702fd4370051aba8e3623b0ab30
정영하 <pd.hyazzang@gmail.com>
2025-05-07 11:29:42 +0900

    🐛 Fix: test fix
    test fix

--- apps/post/models.py
+++ apps/post/models.py
@@ -1,6 +1,7 @@
 from io import BytesIO
 
 import requests
+from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils import timezone
