4bcbb0a5a69a1e60e6d9258a67fe785f67c303e1
LucasKim4321 <xowls0131@naver.com>
2025-05-19 11:11:13 +0900

    🐛fix : url 수정

--- apps/post/urls.py
+++ apps/post/urls.py
@@ -13,9 +13,9 @@ router.register(r"", PostViewSet, basename="post")
 # 3. 최종 URL 패턴
 urlpatterns = [
     # 게시물 관련 엔드포인트
-    path("post", include(router.urls)),
+    path("posts", include(router.urls)),
     path(
-        "post/search",
+        "posts/search",
         PostViewSet.as_view({"get": "posts_search"}),
         name="post-posts_search",
     ),
